c95a11698bfa10b483a69ea97cb8844553052e37
RJ Sheperd <rjsheperd@gmail.com>
2021-02-16 16:53:16 -0800

    Adds GH Action for running tests on push

--- test/triangulum/core_test.clj
+++ test/triangulum/core_test.clj
@@ -1,3 +1,7 @@
 (ns triangulum.core-test
-  (:require [clojure.test :refer [deftest is use-fixtures testing]]
+  (:require [clojure.test :refer [is deftest testing]]
             [triangulum.core :as tri]))
+
+(deftest workflow-test
+  (testing "A simple test"
+    (is (= 2 (+ 1 1)))))
