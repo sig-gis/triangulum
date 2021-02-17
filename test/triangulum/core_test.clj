a04aa08821ba079f7f3691ee71e78f84fabd8f37
RJ Sheperd <rjsheperd@gmail.com>
2021-02-16 16:53:16 -0800

    Attempt at Github Actions

--- test/triangulum/core_test.clj
+++ test/triangulum/core_test.clj
@@ -1,3 +1,7 @@
 (ns triangulum.core-test
-  (:require [clojure.test :refer [deftest is use-fixtures testing]]
-            [triangulum.core :as tri]))
+  (:use [clojure.test])
+  (:require [triangulum.core :as tri]))
+
+(deftest workflow-test
+  (testing "A simple test"
+    (is (= 2 (+ 1 1)))))
