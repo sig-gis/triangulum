4057f5f51ce103afbaa3cc3c918545c494c989da
RJ Sheperd <rjsheperd@gmail.com>
2021-03-05 16:06:42 -0800

    Failing test -- DO NOT MERGE

--- test/triangulum/core_test.clj
+++ test/triangulum/core_test.clj
@@ -5,3 +5,12 @@
 (deftest workflow-test
   (testing "A simple test"
     (is (= 2 (+ 1 1)))))
+
+(defn foo
+  "Adds two numbers"
+  [x y]
+  (+ x y))
+
+(deftest foo-test
+  (testing "A failing test"
+    (is (= 2 (foo)))))
