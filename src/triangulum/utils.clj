c4807a28002695325bfacc1ba48557589e10e234
RJ Sheperd <rjsheperd@gmail.com>
2021-08-24 13:25:28 -0700

    TRI-38 Add tests for triangulum.utils

--- src/triangulum/utils.clj
+++ src/triangulum/utils.clj
@@ -1,5 +1,6 @@
 (ns triangulum.utils
-  (:import java.io.ByteArrayOutputStream)
+  (:import java.io.ByteArrayInputStream
+           java.io.ByteArrayOutputStream)
   (:require [cognitect.transit :as transit]
             [clojure.string    :as str]
             [clojure.data.json :as json]))
@@ -52,13 +53,20 @@
 
 ;; Response building
 
-(defn- clj->transit
+(defn clj->transit
   "Converts a clj body to transit."
   [body]
   (let [out    (ByteArrayOutputStream. 4096)
         writer (transit/writer out :json)]
     (transit/write writer body)
     (.toString out)))
+
+(defn transit->clj
+  "Converts a transit body to clj"
+  [^java.lang.String body]
+  (let [in (ByteArrayInputStream. (.getBytes body))
+        reader (transit/reader in :json)]
+    (transit/read reader)))
 
 (defn data-response
   "Create a response object.
