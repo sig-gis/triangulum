8232bf1cb53836991d7b8b5ce92e20c996f09172
Matt Spencer <sirmspencer@gmail.com>
2021-10-27 11:46:46 -0700

    Update systemd to use current cli code

--- src/triangulum/systemd.clj
+++ src/triangulum/systemd.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io    :as io]
             [clojure.java.shell :as sh]
             [clojure.string     :as str]
-            [clojure.tools.cli  :refer [parse-opts]]
+            [triangulum.cli     :refer [get-cli-options]]
             [triangulum.logging :refer [log-str]]
             [triangulum.utils   :refer [parse-as-sh-cmd]]))
 
@@ -28,26 +28,32 @@ After=network.target
 Type=simple
 User=%s
 WorkingDirectory=%s
-ExecStart=/usr/local/bin/clojure -M:run-server -p %s -P %s -o logs
+ExecStart=/usr/local/bin/clojure -M:run-server %s %s -o logs
 
 [Install]
 WantedBy=multi-user.target
 "))
 
-(defn- enable-systemd [repo path user offset]
-  (if (nil? repo)
-    (println "You must specify a repo with -r when enabling the systemd unit file.")
-    (let [service-name (str "cljweb-" repo)]
+(defn- enable-systemd [{:keys [repo user offset no-http no-https]}]
+  (if (and repo
+           (.exists (io/file repo "deps.edn")))
+    (let [full-path (.getAbsolutePath (io/file repo))
+          service-name (as-> full-path %
+                         (str/split % #"/")
+                         (filter #(re-matches #"\w*" %) %)
+                         (last %)
+                         (str "cljweb-" %))]
       (spit (io/file "/etc/systemd/system/" (str service-name ".service"))
             (format unit-file-template
                     user
-                    (.getAbsolutePath (io/file path repo))
-                    (+ offset 8080)
-                    (+ offset 8443)))
+                    (.getAbsolutePath (io/file repo))
+                    (if no-http ""  (str "-p " (+ offset 8080)))
+                    (if no-https "" (str "-P " (+ offset 8443)))))
       (sh-wrapper "/"
                   {}
                   "systemctl daemon-reload"
-                  (str "systemctl enable " service-name)))))
+                  (str "systemctl enable " service-name)))
+    (println "A repository directory containing deps.edn must be supplied.")))
 
 (defn- disable-systemd [repo]
   (if (nil? repo)
@@ -60,50 +66,41 @@ WantedBy=multi-user.target
       (io/delete-file (io/file "/etc/systemd/system/" (str service-name ".service")) true))))
 
 (defn- systemctl [repo command]
-  (sh-wrapper "/" {} (str "systemctl " command " cljweb-" repo " --all")))
+  (sh-wrapper "/" {} (str "systemctl " (name command) " cljweb-" repo " --all")))
 
 (def ^:private cli-options
-  [["-a" "--all" "Starts, stops, or restarts all cljweb services when specified with the corresponding action."]
-   ["-D" "--disable" "Disable systemd service."]
-   ["-E" "--enable" "Enable systemd service. The service will be created if it doesn't exist."]
-   ["-o" "--offset OFFSET" "Numerical offset from the standard ports of 8080 and 8443."
-    :default 0
-    :parse-fn #(if (int? %) % (Integer/parseInt %))
-    :validate [#(< 0 % 100) "Must be a number between 0 and 100."]]
-   ["-p" "--path PATH" "Alternative path for git repo location."
-    :default "/sig"]
-   ["-r" "--repo REPO" "Repository folder name in /sig or path specified with -p."]
-   ["-X" "--restart" "Restart systemd service."]
-   ["-S" "--start" "Start systemd service."]
-   ["-T" "--stop" "Stop systemd service."]
-   ["-u" "--user USER" "The user account under which the service runs. An unprivileged user is recommended for security reasons."
-    :default "sig"]])
+  {:all      ["-a" "--all" "Starts, stops, or restarts all cljweb services when specified with the corresponding action."]
+   :offset   ["-o"
+              "--offset OFFSET"
+              "Numerical offset from the standard ports of 8080 and 8443. This can be used to run multiple servers on the same machine."
+              :default 0
+              :parse-fn #(if (int? %) % (Integer/parseInt %))
+              :validate [#(< 0 % 100) "Must be a number between 0 and 100."]]
+   :no-http  ["-p" "--no-http" "Disable http."]
+   :no-https ["-P" "--no-https" "Disable https."]
+   :repo     ["-r" "--repo REPO" "Repository folder that contains deps.edn.  With enable, this must be a complete path."]
+   :user     ["-u" "--user USER" "The user account under which the service runs. An unprivileged user is recommended for security reasons."]})
+
+(def ^:private cli-actions
+  {:disable {:description "Disable systemd service."
+             :requires    [:repo]}
+   :enable  {:description "Enable systemd service. The service will be created if it doesn't exist."
+             :requires    [:repo :user]}
+   :restart {:description "Restart systemd service."
+             :requires    [:repo]}
+   :start   {:description "Start systemd service."
+             :requires    [:repo]}
+   :stop    {:description "Stop systemd service."
+             :requires    [:repo]}})
 
 (defn -main
   "The entry point for using the tools provided in systemd.clj."
   [& args]
-  (let [{:keys [options summary errors]} (parse-opts args cli-options)
-        {:keys [all disable enable repo offset path restart start stop user]} options
-        command (or (and restart "restart")
-                    (and start "start")
-                    (and stop "stop"))]
-    (cond
-      (seq errors)
-      (do
-        (run! println errors)
-        (println (str "Usage:\n" summary)))
-
-      enable
-      (enable-systemd repo path user offset)
-
-      disable
-      (disable-systemd repo)
-
-      command
-      (systemctl (if all "*" repo) command)
-
-      :else
-      (do
-        (println "You must indicate which action to take with either --disable, --enable, --start, --stop, or --restart.")
-        (println (str "Usage:\n" summary)))))
+  (let [{:keys [action options]} (get-cli-options args cli-options cli-actions "systemd")
+        {:keys [all repo]} options]
+    (and action
+         (case action
+           :enable  (enable-systemd options)
+           :disable (disable-systemd repo)
+           (systemctl (if all "*" repo) action))))
   (shutdown-agents))
