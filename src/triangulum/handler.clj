dee190c0a7d3e34e061adeebf0cf6757a27897bb
Sif Biri <sif@fastmail.com>
2022-10-20 15:22:18 -0700

    WIP JS/CLJS handling

--- src/triangulum/handler.clj
+++ src/triangulum/handler.clj
@@ -1,10 +1,10 @@
 (ns triangulum.handler
   (:require
-   [clojure.data.json :as json]
-   [clojure.edn       :as edn]
-   [clojure.set       :as set]
-   [clojure.string    :as str]
-   [cognitect.transit  :as transit]
+   [clojure.data.json                  :as json]
+   [clojure.edn                        :as edn]
+   [clojure.set                        :as set]
+   [clojure.string                     :as str]
+   [cognitect.transit                  :as transit]
    [ring.util.codec                    :refer [url-decode]]
    [ring.middleware.absolute-redirects :refer [wrap-absolute-redirects]]
    [ring.middleware.content-type       :refer [wrap-content-type]]
@@ -22,11 +22,12 @@
    [ring.middleware.session.cookie     :refer [cookie-store]]
    [ring.middleware.ssl                :refer [wrap-ssl-redirect]]
    [ring.middleware.x-headers          :refer [wrap-content-type-options
-                                               wrap-frame-options
-                                               wrap-xss-protection]]
+					       wrap-frame-options
+					       wrap-xss-protection]]
    [triangulum.config                  :refer [get-config]]
    [triangulum.logging                 :refer [log-str]]
-   [triangulum.errors                  :refer [nil-on-error]])
+   [triangulum.errors                  :refer [nil-on-error]]
+   [triangulum.utils                   :refer [resolve-foreign-symbol]])
   (:import
    java.io.ByteArrayOutputStream))
 
@@ -36,11 +37,10 @@
   "Converts a clj body to transit."
   [body]
   (let [out    (ByteArrayOutputStream. 4096)
-        writer (transit/writer out :json)]
+	writer (transit/writer out :json)]
     (transit/write writer body)
     (.toString out)))
-#_{:clj-kondo/ignore [:shadowed-var]}
-(def response-type (atom nil))
+#_{:clj-kondo/ignore	[:shadowed-var]}
 
 (defn data-response
   "Create a response object.
@@ -50,22 +50,21 @@
   ([body]
    (data-response body {}))
   ([body {:keys [status type session]
-          :or   {status 200
-                 type   (or @response-type
-                            (reset! response-type (-> (triangulum.config/get-config :server) :response-type)))}
-          :as   params}]
-   (merge (when (contains? params :session) {:session session})
-          {:status  status
-           :headers {"Content-Type" (condp = type
-                                      :edn     "application/edn"
-                                      :transit "application/transit+json"
-                                      :json    "application/json"
-                                      type)}
-           :body    (condp = type
-                      :edn     (pr-str         body)
-                      :transit (clj->transit  body)
-                      :json    (json/write-str body)
-                      body)})))
+	  :or   {status 200
+		 type   (get-config :server :response-type)}}]
+   (cond-> {:status  status
+	    :headers {"Content-Type" (condp = type
+				       :edn     "application/edn"
+				       :transit "application/transit+json"
+				       :json	"application/json"
+				       type)}
+	    :body    (condp = type
+		       :edn	(pr-str body)
+		       :transit (clj->transit body)
+		       :json	(json/write-str body)
+		       body)}
+     (some? session)
+     (merge {:session session}))))
 
 (defn json-response
   ([body] (data-response {:type :json}))
@@ -98,27 +97,27 @@
 (defn wrap-request-logging [handler]
   (fn [request]
     (let [{:keys [uri request-method params]} request
-          param-str                           (pr-str (dissoc params :password :passwordConfirmation))]
+	  param-str                           (pr-str (dissoc params :password :passwordConfirmation))]
       (log-str "Request(" (name request-method) "): \"" uri "\" " param-str)
       (handler request))))
 
 (defn wrap-response-logging [handler]
   (fn [request]
     (let [{:keys [status headers body] :as response} (handler request)
-          content-type                               (headers "Content-Type")]
+	  content-type                               (headers "Content-Type")]
       (log-str "Response(" status "): "
-               (cond
-                 (instance? java.io.File body)
-                 (str content-type " file")
+	       (cond
+		 (instance? java.io.File body)
+		 (str content-type " file")
 
-                 (= content-type "application/edn")
-                 (binding [*print-length* 2] (print-str (edn/read-string body)))
+		 (= content-type "application/edn")
+		 (binding [*print-length* 2] (print-str (edn/read-string body)))
 
-                 (= content-type "application/json")
-                 (binding [*print-length* 2] (print-str (nil-on-error (json/read-str body))))
+		 (= content-type "application/json")
+		 (binding [*print-length* 2] (print-str (nil-on-error (json/read-str body))))
 
-                 :else
-                 (str content-type " response")))
+		 :else
+		 (str content-type " response")))
       response)))
 
 
@@ -127,47 +126,47 @@
 (defn wrap-persistent-session [handler]
   (fn [request]
     (let [{:keys [params session]} request
-          to-update                (select-keys params updatable-session-keys)
-          session                  (apply dissoc session (keys to-update))
-          intersection             (set/intersection (set (keys params)) (set (keys session)))
-          response                 (handler (update request :params merge session))]
+	  to-update                (select-keys params updatable-session-keys)
+	  session                  (apply dissoc session (keys to-update))
+	  intersection             (set/intersection (set (keys params)) (set (keys session)))
+	  response                 (handler (update request :params merge session))]
       (when-not (empty? intersection)
-        (log-str "WARNING! The following params are being overwritten by session values: " intersection))
+	(log-str "WARNING! The following params are being overwritten by session values: " intersection))
       (if (and (contains? response :session)
-               (nil? (:session response)))
-        response
-        (update response :session #(merge session to-update %))))))
+	       (nil? (:session response)))
+	response
+	(update response :session #(merge session to-update %))))))
 
 (defn wrap-exceptions [handler]
   (fn [request]
     (try
       (handler request)
       (catch Exception e
-        (let [{:keys [data cause]} (Throwable->map e)
-              status               (:status data)]
-          (log-str "Error: " cause)
-          (data-response cause {:status (or status 500)}))))))
+	(let [{:keys [data cause]} (Throwable->map e)
+	      status               (:status data)]
+	  (log-str "Error: " cause)
+	  (data-response cause {:status (or status 500)}))))))
 
 (defn parse-query-string [query-string]
   (let [keyvals (-> (url-decode query-string)
-                    (str/split #"&"))]
+		    (str/split #"&"))]
     (reduce (fn [params keyval]
-              (->> (str/split keyval #"=")
-                   (map edn/read-string)
-                   (apply assoc params)))
-            {}
-            keyvals)))
+	      (->> (str/split keyval #"=")
+		   (map edn/read-string)
+		   (apply assoc params)))
+	    {}
+	    keyvals)))
 
 (defn wrap-edn-params [handler]
-  (fn [{:keys [content-type request-method query-string body params] :as request}]
+  (fn [{:keys	[content-type request-method query-string body params]	:as	request}]
     (if (= content-type "application/edn")
       (let [get-params  (when (and (= request-method :get)
-                                   (not (str/blank? query-string)))
-                          (parse-query-string query-string))
-            post-params (when (and (= request-method :post)
-                                   (not (nil? body)))
-                          (edn/read-string (slurp body)))]
-        (handler (assoc request :params (merge params get-params post-params))))
+				   (not (str/blank? query-string)))
+			  (parse-query-string query-string))
+	    post-params (when (and (= request-method :post)
+				   (not (nil? body)))
+			  (edn/read-string (slurp body)))]
+	(handler (assoc request :params (merge params get-params post-params))))
       (handler request))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -193,16 +192,23 @@
       wrap-nested-params
       wrap-multipart-params
       wrap-params
-      (wrap-session {:store (cookie-store {:key (string-to-bytes (get-config :session-key))})})
+      (wrap-session {:store
+		     (cookie-store {:key
+				    (string-to-bytes (get-config :server :session-key))})})
       wrap-absolute-redirects
       (wrap-resource "public")
       wrap-content-type
       (wrap-default-charset "utf-8")
       wrap-not-modified
-      (wrap-xss-protection true {:mode :block})
+      (wrap-xss-protection true {:mode	:block})
       (wrap-frame-options :sameorigin)
       (wrap-content-type-options :nosniff)
       wrap-response-logging
       wrap-gzip
       wrap-exceptions
       (optional-middleware wrap-reload reload?)))
+
+(def development-app (create-handler-stack
+		      (-> (get-config :server :handler) resolve-foreign-symbol)
+		      false
+		      true))
--- src/triangulum/views.clj
+++ src/triangulum/views.clj
@@ -10,7 +10,9 @@
    [triangulum.config   :refer [get-config]]
    [triangulum.database :refer [call-sql]]
    [triangulum.git      :refer [current-version]]
-   [triangulum.logging :refer [log-str]])
+   [triangulum.logging  :refer [log-str]]
+   [triangulum.errors   :refer [nil-on-error]]
+   [triangulum.utils    :refer [resolve-foreign-symbol kebab->snake]])
   (:import
    java.io.ByteArrayOutputStream))
 
@@ -18,46 +20,7 @@
   (let [pieces (str/split kebab #"-")]
     (apply str (first pieces) (map str/capitalize (rest pieces)))))
 
-(defn find-bundle-js-files [page]
-  (let [src-file (format "src/js/%s.jsx" page)
-        manifest (if (= "dev" (get-config :server :mode))
-                   (loop [resp    (try
-                                    (client/get "http://localhost:5173/manifest.json"
-                                                {:accept :json})
-                                    (catch Exception e nil))
-                          counter 0]
-                     (cond
-                       resp
-                       (-> resp
-                           :body
-                           (json/read-str)
-                           (get src-file))
-
-                       (> counter 50)
-                       (throw (ex-info "can't find manifest.json" {}))
-
-                       :else
-                       (do (Thread/sleep 200)
-                           (recur resp (inc counter)))))
-                   (-> (slurp "dist/public/manifest.json")
-                       (json/read-str)
-                       (get src-file)))
-
-        entrypoint-file (str "/" (get manifest "file"))
-        asset-files     (mapv #(str/replace % #"^_" "/assets/")
-                              (get manifest "imports"))]
-    (conj asset-files entrypoint-file)))
-
-(defn find-bundle-css-files []
-  (let [manifest        (-> (slurp "dist/public/manifest.json")
-                            (json/read-str))
-        asset-css-files (->> manifest
-                             (filter (fn [[k _v]] (str/includes? k ".css")))
-                             (map (fn [[_k v]] (str "/" (get v "file")))))]
-    asset-css-files))
-
-;; TODO: better naming
-(defn find-app-js []
+(defn find-cljs-app-js []
   (as-> (slurp "target/public/cljs/manifest.edn") app
     (edn/read-string app)
     (get app "target/public/cljs/app.js")
@@ -65,71 +28,109 @@
     (last app)
     (str "/cljs/" app)))
 
-(defn head [bundle-js-files bundle-css-files lang]
-  (let [app-config                           (get-config :app)
-        {:keys [title description keywords]} app-config
-        css-files                            (get-in app-config [:views :css-files])
-        js-files                             (get-in app-config [:views :js-files])]
-    [:head
-     [:title (title lang)]
-     [:meta {:charset "utf-8"}]
-     [:meta {:name "viewport" :content "width=device-width, user-scalable=no"}]
-     [:meta {:name "description" :content (description lang)}]
-     [:meta {:name "keywords" :content (keywords lang)}]
-                                        ; Favicon entries
-     [:link {:rel "apple-touch-icon" :sizes "180x180" :href "/favicon/apple-touch-icon.png"}]
-     [:link {:rel "icon" :type "image/png" :sizes "32x32" :href "/favicon/favicon-32x32.png"}]
-     [:link {:rel "icon" :type "image/png" :sizes "16x16" :href "/favicon/favicon-16x16.png"}]
-     [:link {:rel "manifest" :href "/favicon/site.webmanifest"}]
-     [:link {:rel "mask-icon" :color "#5bbad5" :href "/favicon/safari-pinned-tab.svg"}]
-     [:link {:rel "shortcut icon" :href "/favicon/favicon.ico"}]
-     [:meta {:name "msapplication-TileColor" :content "#ffc40d"}]
-     [:meta {:name "msapplication-config" :content "/favicon/browserconfig.xml"}]
-     [:meta {:name "theme-color" :content "#ffffff"}]
-                                        ; end Favicon
+(defn find-manifest [src-file]
+  (if (= "dev" (get-config :server :mode))
+    (loop [resp    (nil-on-error
+                    (client/get "http://localhost:5173/manifest.json"
+                                {:accept :json}))
+           counter 0]
+      (cond
+        resp
+        (-> resp
+            :body
+            (json/read-str))
 
-     (when-let [ga-id (get-config :ga-id)]
-       (list [:script {:async true :src (str "https://www.googletagmanager.com/gtag/js?id=" ga-id)}]
-             [:script (str "window.dataLayer = window.dataLayer || []; function gtag() {dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" ga-id "', {'page_location': location.host + location.pathname});")]))
+        (> counter 50)
+        (throw (ex-info "can't find manifest.json" {}))
+
+        :else
+        (do (Thread/sleep 200)
+            (recur resp (inc counter)))))
+    (-> (slurp "dist/public/manifest.json")
+        (json/read-str))))
+
+;; TODO: maybe rename :client-init to a better name
+(defn cljs-project? []
+  (get-config :app :client-init))
+
+(defn find-bundle-asset-files
+  "Return a map of JS and CSS asset paths for React/Vite JS projects. Return nil
+  for CLJS projects."
+  [page]
+  (when-not (cljs-project?)
+    (let [src-file           (format "src/js/%s.jsx" page)
+          manifest           (find-manifest src-file)
+          js-entrypoint-file (str "/" (get-in manifest [src-file "file"]))
+          js-asset-files     (mapv #(str/replace % #"^_" "/assets/")
+                                   (get-in manifest [src-file "imports"]))
+          css-asset-files    (reduce-kv (fn [acc k v]
+                                          (if (str/includes? k ".css")
+                                            (conj acc (str "/" (get v "file")))
+                                            acc))
+                                        []
+                                        manifest)]
+      {:js-files  (conj js-asset-files js-entrypoint-file)
+       :css-files css-asset-files})))
+
+(defn head [{:keys [bundle-js-files bundle-css-files lang]}]
+  (let [{:keys [title description keywords extra-head-tags gtag-id static-css-files static-js-files]} (get-config :app)]
+    [:head
+     [:title (get title lang "")]
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "description" :content (get description lang "")}]
+     [:meta {:name "keywords" :content (get keywords lang "")}]
+     (seq extra-head-tags)
+     (when gtag-id
+       (list [:script {:async true :src (str "https://www.googletagmanager.com/gtag/js?id=" gtag-id)}]
+             [:script (str "window.dataLayer = window.dataLayer || [];"
+                           "function gtag() { dataLayer.push(arguments); }"
+                           "gtag('js', new Date());"
+                           "gtag('config', '" gtag-id "', {'page_location': location.host + location.pathname});")]))
      (apply include-css
-            (concat css-files
+            (concat static-css-files
                     bundle-css-files))
-     (apply include-js (concat js-files [(find-app-js)]))
-     (if (= "dev" (get-config :server :mode))
+     (apply include-js static-js-files)
+     (cond
+       ;; CLJS app
+       (cljs-project?)
+       (include-js (find-cljs-app-js))
+
+       ;; JS app/prod
+       (= "prod" (get-config :server :mode))
+       (map (fn [f] [:script {:type "module" :src f}])
+            (butlast bundle-js-files))
+
+       ;; JS app/dev
+       :else
        (list
-         [:script {:type "module"}
-          "// import RefreshRuntime from 'http://localhost:5173/@react-refresh'
+        [:script {:type "module"}
+         "// import RefreshRuntime from 'http://localhost:5173/@react-refresh'
          // RefreshRuntime.injectIntoGlobalHook(window)
-         window.$RefreshReg$ = () => {}
+         window.$Refresh Reg$ = () => {}
          window.$RefreshSig$ = () => (type) => type
          window.__vite_plugin_react_preamble_installed__ = true"]
-         [:script {:type "module" :src "http://localhost:5173/@vite/client"}]
-         (map (fn [f] [:script {:type "module" :src (str "http://localhost:5173" f)}])
-              bundle-js-files))
-       (map (fn [f] [:script {:type "module" :src f}])
-            bundle-js-files))]))
+        [:script {:type "module" :src "http://localhost:5173/@vite/client"}]
+        (map (fn [f] [:script {:type "module" :src (str "http://localhost:5173" f)}])
+             (butlast bundle-js-files))))]))
 
 (defn uri->page [uri]
   (->> (str/split uri #"/")
        (remove str/blank?)
        (first)
-       (kebab->camel)))
-
-
+       ((fnil kebab->camel "home"))))
 
 (defn client-init [entry-file params]
-  (let [client-init (-> (get-config :app ) :client-init)
-        js-params   (-> params
-                        (assoc
-                          :mapboxToken     (get-config :mapbox-token)
-                          :mapquestKey     (get-config :mapquest-key)
-                          :versionDeployed (current-version))
-                        (json/write-str))
-        script-str  (str (when entry-file "import {pageInit} from \"") entry-file "\";"
-                         "window.onload = function () {" client-init"(" js-params "); };")
-        ]
-    [:script {:type "module"}
-     script-str]))
+  (let [js-params (-> params
+                      (assoc :versionDeployed (current-version))
+                      (merge (get-config :app :client-keys))
+                      (json/write-str))]
+    (if-let [cljs-init (get-config :app :client-init)]
+      ;; CLJS app
+      [:script {:type "text/javascript"}
+       (format "window.onload = function () { %s(%s); };" (-> cljs-init name kebab->snake) js-params)]
+      ;; JS app
+      [:script {:type "module"}
+       (format "import { pageInit } from \"%s\"; window.onload = function () { pageInit(%s); };" entry-file js-params)])))
 
 (defn- announcement-banner []
   (let [announcement (-> (slurp "announcement.txt")
@@ -176,33 +177,36 @@
          [:path {:d "M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83
                      11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"}]]]])))
 
+;; TODO: Update find-bundle-asset-files to return values for CLJS projects.
+(defn get-response-params [uri request]
+  (let [page          (uri->page uri)
+        asset-files   (find-bundle-asset-files page)
+        get-user-lang (some-> (get-config :app :get-user-lang) resolve-foreign-symbol)
+        lang          (if get-user-lang
+                        (get-user-lang request)
+                        :en)]
+    {:bundle-js-files  (:js-files asset-files)
+     :bundle-css-files (:css-files asset-files)
+     :lang             lang}))
 
-;; TODO: infer form the config and have one function name (render-page)
 (defn render-page [uri]
   (fn [request]
-    (let [page             (uri->page uri)
-          bundle-js-files  (find-bundle-js-files page)
-          bundle-css-files (find-bundle-css-files)
-          user-id          (-> request :params :userId)
-          user             (first (call-sql "get_user_information" user-id))
-          lang             (:default-lang user :en)]
+    (let [response-params (get-response-params uri request)]
       {:status  200
        :headers {"Content-Type" "text/html"}
        :body    (html5
-                  (head (butlast bundle-js-files) bundle-css-files lang)
-                  [:body
-                   (if (seq bundle-js-files)
-                     [:section
-                      ;; TODO These will be moved to the front end for better UX.
-                      (when-let [flash-message (get-in request [:params :flash_message])]
-                        ;; TODO: we don't depend on class definition anymore
-                        [:p {:class "alert"} flash-message])
-                      (when (.exists (io/as-file "announcement.txt"))
-                        (announcement-banner))
-                      [:div#main-container]]
-                     [:label "No JS bundle files found. Check if your bundler is running, or wait for it to finish compiling."])
-                   (client-init (last bundle-js-files) (:params request) )])})))
-
+                 (head response-params)
+                 [:body
+                  [:section
+                   ;; TODO These will be moved to the front end for better UX.
+                   (when-let [flash-message (get-in request [:params :flash_message])]
+                     ;; FIXME: We need to write class definition in css file and serve it by default
+                     [:p {:class "alert"} flash-message])
+                   (when (.exists (io/as-file "announcement.txt"))
+                     (announcement-banner))
+                   [:div#app]]
+                  (client-init (-> response-params :bundle-js-files last)
+                               (:params request))])})))
 
 (defn not-found-page [request]
   (-> request
