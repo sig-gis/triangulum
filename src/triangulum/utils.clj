07d2a22b773369ead24b1c5d1cf3f219c9a1ca4b
Oliver-BE <obaldwinedwards@gmail.com>
2022-12-27 16:22:21 -0600

    Add a new sh-wrapper function

--- src/triangulum/utils.clj
+++ src/triangulum/utils.clj
@@ -63,9 +63,20 @@
 
 (def ^:private path-env (System/getenv "PATH"))
 
+(defn sh-exec-with
+  "Provides a given path and environment to one bash command,
+  and parses the output, creating an array as described in `parse-as-sh-cmd`.
+  Returns a map in the following format: {:exit 0 :out 'Output message\n' :err ''}"
+  [dir env command]
+  (sh/with-sh-dir dir
+    (sh/with-sh-env (merge {:PATH path-env} env)
+      (apply sh/sh (parse-as-sh-cmd command)))))
+
 (defn sh-wrapper
   "Provides a given path and environment to a set of bash commands,
-  and parses the output, creating an array as described in `parse-as-sh-cmd`."
+  and parses the output, creating an array as described in `parse-as-sh-cmd`.
+  Returns a string that combines all of the standard error (and optionally
+  the standard out when `verbose` is true) from running the set of commands."
   [dir env verbose & commands]
   (sh/with-sh-dir dir
     (sh/with-sh-env (merge {:PATH path-env} env)
