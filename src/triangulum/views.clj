86c0c65e1efbbbe351a12f3b37e03aab60c0ea65
drew verlee <drew.verlee@gmail.com>
2025-04-08 16:31:22 -0500

    the key would still have the original file path.

--- src/triangulum/views.clj
+++ src/triangulum/views.clj
@@ -40,7 +40,8 @@
   []
   (as-> (slurp (io/resource "public/cljs/manifest.edn")) app
     (edn/read-string app)
-    (get app "public/cljs/app.js")
+    ;;manifest is put in target dir. but it's read on the classpath.
+    (get app "target/public/cljs/app.js")
     (str/split app #"/")
     (last app)
     (str "cljs/" app)))
